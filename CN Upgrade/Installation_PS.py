389225840352ebcf76503bd9c017664eefaed958
sobiradar49 <136556909+sobiradar49@users.noreply.github.com>
2023-07-06 15:02:57 +0530

    Update Installation_PS.py

--- CN Upgrade/Installation_PS.py
+++ CN Upgrade/Installation_PS.py
@@ -44,7 +44,132 @@ def exec_cmd(cmd):
 
     return stdout.decode("utf-8") + stderr.decode("utf-8")
 
+def exec_cmduntar(cmd,path):
+    # Execute in a sub-process and capture output.
+    os.chdir(path)
+    print(os.getcwd())
+    proc = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+    )
+    stdout, stderr = proc.communicate()
+
+    return stdout.decode("utf-8") + stderr.decode("utf-8")
+
+
+def clear_db():
+    if Clear_data_from_mnt_folders.casefold() == "yes" or Clear_data_from_mnt_folders.casefold() == "y":
+        isExist = os.path.exists("/mnt/data5")
+        if isExist == False:
+            os.mkdir("/mnt/data5")
+        clear_db = exec_cmd("rm -rf /mnt/data0/*; rm -rf /mnt/data1/*; rm -rf /mnt/data2/*; rm -rf /mnt/data5/*")
+        print(clear_db)
+        print(Fore.RED+"\n Data has been cleared from mnt folders \n")
+    else:
+        print (Fore.GREEN+"\n Data hasn't been cleared from mnt floders \n")
+
+def copy_load(basepath,path):
+    # Execute in a sub-process and capture output.
+    os.chdir(path+"/TRILLIUM_5GCN_CNF_REL_"+new_image_version+"/common/tools/install")
+    print(os.getcwd())
+    proc = subprocess.Popen(
+        basepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+    )
+    stdout, stderr = proc.communicate()
+
+    return stdout.decode("utf-8") + stderr.decode("utf-8")
+
+def load_fun(new_image_version,path):
+    os.chdir(path)
+    print(os.getcwd())
+    os.system('./load.sh {}' .format(str(new_image_version)))
+    time.sleep(5)
+def ps_ingress(cluster_ip,file_load,path,new_image_version):
+    os.chdir(path+"/TRILLIUM_5GCN_CNF_REL_"+new_image_version+"/platform-services/scripts")
+    with open (file_load) as read_file:
+        node = yaml.safe_load(read_file)
+        node['nginx-ingress']['nginx_ingress']['externalIP'] = cluster_ip
+        with open(file_load, 'w') as yaml_file:
+            data = yaml_file.write(yaml.dump(node, sort_keys=False, default_flow_style=False))
+            file_load =data
+
+
+def ps_el(cluster_ip,file_load,path,new_image_version):
+    os.chdir(path+"/TRILLIUM_5GCN_CNF_REL_"+new_image_version+"/platform-services/scripts")
+    with open (file_load) as read_file:
+        node = yaml.safe_load(read_file)
+        print(node)
+        node['fluentd']['elasticHost'] = cluster_ip
+        node['kibana']['elasticHost'] = cluster_ip
+        with open(file_load, 'w') as yaml_file:
+            data = yaml_file.write(yaml.dump(node, sort_keys=False, default_flow_style=False))
+            file_load =data
+def platform_services(path,new_image_version):
+    os.chdir(path+"/TRILLIUM_5GCN_CNF_REL_"+new_image_version+"/platform-services/scripts")
+    os.system("sh install_ps.sh")
+    time.sleep(10)
+    os.system("sh install_mongodb.sh")
+    time.sleep(90)
+    os.system("sh addmongoreplica.sh")
+    time.sleep(25)
+    os.system("sh addmongoreplica.sh")
+    time.sleep(10)
+    ns = exec_cmd("kubectl get ns")
+    print(Fore.BLUE+ns)
+    if "mongodb" and "radisys-ps1" and "ingress-nginx" in ns:
+        print(Fore.BLUE+"\n Platform services are installed sucessfully \n")
+    else:
+        print(Fore.RED+"\n FAILED TO INSTALL PLATFORM SERVICES \n")
+        exit()
+
+def cs_function(file_load, default_nf_version, new_image_version,path):
+        os.chdir(path+"/TRILLIUM_5GCN_CNF_REL_"+new_image_version+"/common-services/scripts")
+        fin = open(file_load, "rt")
+        #read file contents to string
+        data = fin.read()
+        #replace all occurrences of the required string_function
+        data = data.replace(default_nf_version, new_image_version)
+        #close the input file
+        fin.close()
+        #open the input file in write mode
+        fin = open(file_load, "wt")
+        #overrite the input file with the resulting data
+        fin.write(data)
+        #close the file
+        fin.close()
+
+def common_service(path,new_image_version):
+    os.chdir(path+"/TRILLIUM_5GCN_CNF_REL_"+new_image_version+"/common-services/scripts")
+    os.system("sh install_cs.sh")
+    time.sleep(1)
+    ns = exec_cmd("kubectl get ns")
+    print(Fore.BLUE+ns)
+    if "radisys-cs1" in ns:
+        print(Fore.BLUE+"\n Common services are installed sucessfully \n")
+    else:
+        print(Fore.RED+"\n FAILED TO INSTALL COMMON SERVICES \n")
+        exit()
+
 
 print(Fore.BLUE+"\n Clear data from mnt folders based on user input \n")
 cleardb = clear_db()
 
+print(Fore.BLUE+"\n Starting the unzip process for new package \n")
+untar = ("tar -xvf TRILLIUM_5GCN_CNF_REL_"+new_image_version+".tar.gz")
+exec_cmduntar(untar,path)
+basepath = "cp load.sh"+" "+path
+print(Fore.BLUE+"\n Copying the load.sh script to base path \n")
+copy_load(basepath,path)
+print(Fore.BLUE+"\n Loading the new images \n")
+load_fun(new_image_version,path)
+
+print(Fore.BLUE+"\n Starting the Platform service installation \n")
+ps_ingress(cluster_ip,file_load_10,path,new_image_version)
+ps_el(cluster_ip,file_load_11,path,new_image_version)
+platform_services(path,new_image_version)
+
+
+
+######################## CS Installation #######################
+print(Fore.BLUE+"\n Starting the Common service installation \n")
+cs_function(file_load_9,default_nf_version,new_image_version,path)
+common_service(path,new_image_version)
